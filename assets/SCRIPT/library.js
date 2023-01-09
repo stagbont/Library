fa66be3c1ca85ce5d5257bbb2a2d6c4acc8c9927
stagbont <stanleyagbontor63@gmail.com>
2023-01-09 19:35:24 +0000

    Added the getInputData from the form Functionality

--- assets/SCRIPT/library.js
+++ assets/SCRIPT/library.js
@@ -22,5 +22,28 @@ function Book(title, author, pages, status) {
 }
 
 function addBookToLibrary() {
-
+   
 }
+
+function getInputData() {
+    const form = document.querySelector(".modal"),
+         titleInput = document.querySelector("#book__title"),
+         authorInput = document.querySelector("#book__author"),
+         pagesInput = document.querySelector("#book__pages"),
+         readInput = document.querySelector("#book__read");
+
+    
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault();
+
+        let title = titleInput.value;
+        let author = authorInput.value;
+        let pages = pagesInput.value;
+        let status = readInput.value;
+
+        let book = new Book(title, author, pages, status);
+
+        myLibrary.push(book);
+    })
+}
+
